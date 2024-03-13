8c1e6ec384bf2c7d9db38dd5f9614ed5244ae055
Denis Burguto <65451582GMAIL.COM>
2024-03-13 18:23:58 +0300

    almost done

--- task_3.py
+++ task_3.py
@@ -1,14 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import CSRFProtect
 from forms import RegistrationForm
-from models import User, db
+from models import db, User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = b'5b1aa6a7a0ea7d0b36cd5499146ab901ee959c04ca90531b4025169cf29a73ff'
 csrf = CSRFProtect(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../../mydatabase.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../instance/mydatabase.db'
 db.init_app(app)
-
 
 
 @app.route('/')
@@ -20,25 +19,19 @@ def index():
 def register():
     form = RegistrationForm()
     if request.method == 'POST' and form.validate():
-        name = form.user_name.data
-        surname = form.user_surname.data
-        email = form.email.data
-        password = form.password.data
         db.session.add(User(user_name=form.user_name.data, user_surname=form.user_surname.data, email=form.email.data,
                             hashed_psw=form.password.data))
         db.session.commit()
-        print('OK')
-        print(email, password, name, surname)
+        users = User.query.all()
+        context = {'users': users, 'registered': True}
+        return render_template('index.html', **context)
     return render_template('register.html', form=form)
-
 
 
 @app.cli.command('create_db')
 def one_time_run():
     db.create_all()
 
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
